42f83594bc156db2807f7db5f888e120a809ba55
Emilia <81705094+isemilia@users.noreply.github.com>
2026-09-07 14:25:53 +0200

    Implement sign in

--- src/shared/schemas/auth.ts
+++ src/shared/schemas/auth.ts
@@ -4,4 +4,10 @@ export const signUpSchema = z.object({
     name: z.string(),
     email: z.string(),
     password: z.string(),
+});
+
+
+export const signInSchema = z.object({
+    email: z.string(),
+    password: z.string(),
 })
--- src/shared/utils/password-hasher.ts
+++ src/shared/utils/password-hasher.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-export const hashPassword = (password: string, salt: string) => {
+export const hashPassword = (password: string, salt: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         crypto.scrypt(password.normalize(), salt, 64, (error, hashed) => {
             if (error) {
@@ -13,4 +13,17 @@ export const hashPassword = (password: string, salt: string) => {
 
 export const generateSalt = () => {
     return crypto.randomBytes(16).toString('hex').normalize()
+}
+
+export const comparePasswords = async (
+    password: string,
+    salt: string,
+    hashedPassword: string
+) => {
+    const hashedUserInput = await hashPassword(password, salt);
+
+    return crypto.timingSafeEqual(
+        Buffer.from(hashedUserInput, 'hex'),
+        Buffer.from(hashedPassword, 'hex')
+    );
 }
